fac058fb6f73be2092deed0538a6427f44078e47
GUILLERMOBPY <guillermo-benitez@outlook.com>
2023-10-03 17:17:43 -0300

    Implementacion del Output

--- src/app/dbz/components/add-character/add-character.component.ts
+++ src/app/dbz/components/add-character/add-character.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
-  selector: 'app-add-character',
+  selector: 'dbz-add-character',
   templateUrl: './add-character.component.html',
-  styleUrls: ['./add-character.component.css']
+  styleUrls: ['./add-character.component.css'],
 })
 export class AddCharacterComponent {
-
+  @Output()
+  onNewCharacter: EventEmitter<Character> = new EventEmitter();
+  character: Character;
+  constructor() {
+    this.character = {
+      name: '',
+      power: 0,
+    };
+  }
+  emiteCharacter(): void {
+    if (this.character.name.length === 0) return;
+    this.onNewCharacter.emit(this.character);
+    this.character = {
+      name: '',
+      power: 0,
+    };
+  }
 }
--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
-  selector: 'app-dbz-list',
+  selector: 'dbz-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css'],
 })
-export class ListComponent {}
+export class ListComponent {
+  @Input()
+  characterList: Character[];
+  constructor() {
+    this.characterList = [];
+  }
+
+  @Output()
+  onDeleteId: EventEmitter<number> = new EventEmitter();
+  onDeleteCharacter(index: number): void {
+    this.onDeleteId.emit(index);
+  }
+}
