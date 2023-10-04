13eb7b1a55716ef1eb2dba80cfb7b2e61f6b266d
GUILLERMOBPY <guillermo-benitez@outlook.com>
2023-10-04 10:40:28 -0300

    Fin Sección 5

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,10 +1,21 @@
-import { DBZService } from './../services/dbz.service';
 import { Component } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
+import { DBZService } from './../services/dbz.service';
 
 @Component({
   selector: 'app-dbz-main-page',
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
-  constructor(public dbzService: DBZService) {}
+  constructor(private dbzService: DBZService) {}
+
+  get characters(): Character[] {
+    return [...this.dbzService.characters];
+  }
+  onDeleteId(id: string): void {
+    this.dbzService.onDeleteId(id);
+  }
+  onNewCharacter(character: Character): void {
+    this.dbzService.onNewCharacter(character);
+  }
 }
